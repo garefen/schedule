6b065c591ad46a0c2557c7cd2d5fdb0d955b7e47
Gabriel Sarti <gabrielbsarti@gmail.com>
2020-02-18 14:31:00 -0300

    removed server

--- src/server/constrais.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    week: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-    months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
-}
