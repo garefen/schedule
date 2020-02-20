431d1db896a6e47a133ca92313013ff83a2d2d70
Gabriel Sarti <gabrielbsarti@gmail.com>
2020-02-20 12:26:47 -0300

    fix api call

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 require('dotenv/config');
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
-    // baseURL: 'https://rotina-escolar-backend.herokuapp.com/'
+    // baseURL: 'http://localhost:3333'
+    baseURL: 'https://rotina-escolar-backend.herokuapp.com/'
 });
 
 export default api;
